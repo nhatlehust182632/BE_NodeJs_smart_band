dfb993e8b98b34a365d464cf7901f711aee8e7bf
nhatlehust182632 <le.ntn182632@gmail.com>
2026-06-21 23:51:35 +0700

    auto connect

--- src/sql/devices/infoDevice.select.sql
+++ src/sql/devices/infoDevice.select.sql
@@ -1,8 +1,21 @@
-SELECT d.model_name
-FROM users u
-JOIN user_devices ud 
+SELECT
+    d.device_id,
+    d.device_name,
+    d.model_name,
+    uad.is_connected,
+    uad.connected_at,
+    uad.last_seen_at
+FROM users AS u
+LEFT JOIN user_active_devices AS uad
+    ON uad.user_id = u.id
+   AND uad.is_connected = 1
+LEFT JOIN user_devices AS ud
     ON ud.user_id = u.id
-JOIN devices d 
+   AND ud.device_id = uad.device_id
+   AND ud.pairing_status = 1
+   AND ud.unpaired_at IS NULL
+LEFT JOIN devices AS d
     ON d.id = ud.device_id
 WHERE u.id = ?
-  AND ud.pairing_status = 1;
+ORDER BY uad.last_seen_at DESC, uad.connected_at DESC
+LIMIT 1;
--- src/sql/users/login.select.sql
+++ src/sql/users/login.select.sql
@@ -1,17 +1,20 @@
-SELECT u.id,
-       u.full_name,
-       d.device_id,
-       d.device_name
+SELECT
+    u.id,
+    u.full_name,
+    d.device_id,
+    d.device_name
 FROM users AS u
-LEFT JOIN user_devices AS ud
-    ON u.id = ud.user_id
-   AND ud.pairing_status = 1
-   AND ud.unpaired_at IS NULL
 LEFT JOIN user_active_devices AS uad
     ON uad.user_id = u.id
-   AND uad.device_id = ud.device_id
    AND uad.is_connected = 1
+LEFT JOIN user_devices AS ud
+    ON ud.user_id = u.id
+   AND ud.device_id = uad.device_id
+   AND ud.pairing_status = 1
+   AND ud.unpaired_at IS NULL
 LEFT JOIN devices AS d
-    ON d.id = uad.device_id
+    ON d.id = ud.device_id
 WHERE u.id = ?
-  AND u.password_hash = ?;
+  AND u.password_hash = ?
+ORDER BY uad.last_seen_at DESC, uad.connected_at DESC
+LIMIT 1;
